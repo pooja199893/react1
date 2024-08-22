72c1fcaf219af91079aa2d53e857c1bd5b71e949
pooja199893 <poojacrane15111998@gmail.com>
2024-08-22 17:23:07 +0530

    updates

---
+++ myapp/src/components/21-08/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  console.log(counter, "counter");
+
+  function Increment() {
+    setCounter(counter + 1);
+  }
+   function Decrement(){
+    setCounter(counter - 1)
+   } 
+   function Reset(){
+    setCounter(0)
+   }
+  return (
+    <div>
+      <h1>Counter : {counter}</h1>
+      <button onClick={Increment}>+</button>
+      <button onClick={Decrement}>-</button>
+      <button onClick={Reset}>RESET</button>
+    </div>
+  );
+}
+
+export default Counter;
